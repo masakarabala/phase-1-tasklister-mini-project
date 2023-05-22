a8909746f9e79cec5543d31f92a57addadec6795
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-05-22 19:53:48 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form=document.getElementById('create-task-form')
+  
+  form.addEventListener('submit',function(e){
+    e.preventDefault();
+    buildtoDo(e.target.new_task_description.value)
+
+  })
 });
+
+function buildtoDo(todo){
+  const p=document.createElement("p")
+  p.textContent=todo;
+  document.getElementById('list').appendChild(p)
+  // create button
+  const btn=document.createElement('button')
+  btn.textContent=' x'
+  p.appendChild(btn)
+
+  btn.addEventListener('click',function handelDelet(e){
+     e.target.parentNode.remove()
+  })
+}
